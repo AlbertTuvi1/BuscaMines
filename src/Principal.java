f7920ba1be072b0b5381aa8769ab1fac8c6e172e
tuvi <atuvi4@gmail.com>
2023-01-17 15:27:51 +0100

    menu2

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,36 @@
 public class Principal {
     public static void main(String[] args) {
-    int menu = menu();    
-        
+    int menu = menu();
+    boolean sortir = false;
+
+    switch (menu){
+        case 1:
+
+
+        case 2:
+
+    }
     }
 
-    private static int menu() {
+    public static int menu() {
         int opcio = 0;
         System.out.println("1.Entrar el joc de Buscamines");
         System.out.println("2.Sortir");
         return opcio;
     }
+
+    public static void taulell(){
+        System.out.println("Selecciona la dificultat");
+        System.out.println("1.Facil");
+        System.out.println("2.Mitja");
+        System.out.println("3.Dificil");
+        }
+
+    }
+
+
+
+
 
 
 }
