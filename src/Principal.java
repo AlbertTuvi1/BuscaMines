78e7ad90dc10a25d77919075003c0ad3c0eb2f4a
tuvi <atuvi4@gmail.com>
2023-01-17 15:22:42 +0100

    menu

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+    public static void main(String[] args) {
+    int menu = menu();    
+        
+    }
+
+    private static int menu() {
+        int opcio = 0;
+        System.out.println("1.Entrar el joc de Buscamines");
+        System.out.println("2.Sortir");
+        return opcio;
+    }
+
+
+}
